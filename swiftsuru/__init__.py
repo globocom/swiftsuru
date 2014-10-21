254a2f012dfe58a907812ff4bfb45a5b1adc931c
Flavia Missi <flaviamissi@gmail.com>
2014-10-21 18:12:57 -0200

    moving init stuff to main

--- swiftsuru/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-import api
-
-app = Flask(__name__)
-
-app.register_blueprint(api.api)
---
+++ swiftsuru/__main__.py
@@ -0,0 +1,8 @@
+from flask import Flask
+import api
+
+app = Flask(__name__)
+
+app.register_blueprint(api.api)
+
+app.run()
