cad4be35bb6310ba713743a3a88e0275bfa3f1a8
Flavia Missi <flaviamissi@gmail.com>
2014-10-22 16:23:35 -0200

    starts swift client wrap

---
+++ swiftsuru/swift_client.py
@@ -0,0 +1,27 @@
+import swiftclient
+from swiftsuru.conf import AUTH_URL, USER, KEY
+
+
+class SwiftClient(object):
+    """
+    python-swiftclient abstraction.
+    This class authenticates the user and stores an authenticated connection
+    with Swift's server.
+    It also abstracts swiftclient actions, so you don't have to store the connection
+    yourself or perform the actions on top of it, for example, what could be:
+        cli = SwiftClient() # do some dirty work for you
+        cli.conn.create_account(<...>)
+    This is kind of ugly, to make it cleaner we abstract the connection for you, e.g:
+        cli = SwiftClient()
+        cli.create_account(<...>) # much better!
+    """
+
+    """
+    Authenticates on swift with AUTH_URL, USER, and KEY from conf.py.
+    Gets auth information for next API calls via conn.get_auth() and
+    a authenticated client connection for performing actions.
+    """
+    def __init__(self):
+        conn = swiftclient.client.Connection(authurl=AUTH_URL, user=USER, key=KEY)
+        auth_url, auth_token = conn.get_auth()
+        self.conn = swiftclient.client.Connection(preauthurl=auth_url, preauthtoken=auth_token)
---
+++ tests/test_swift_client.py
@@ -0,0 +1,33 @@
+import unittest
+from mock import patch, call
+from swiftsuru.swift import SwiftClient
+from swiftsuru.conf import AUTH_URL, USER, KEY
+
+
+class SwiftClientTest(unittest.TestCase):
+
+    def setUp(self):
+        self.url = "swifturl.com/AUTH_user"
+        self.token = "AUTH_tk65ade122e45449f2aefdffcf72a30bd7"
+
+    @patch("swiftsuru.swift.swiftclient")
+    def test_init_should_obtain_token(self, swiftclient):
+        swiftclient.client.Connection.return_value.get_auth.return_value = (self.url, self.token)
+        cli = SwiftClient()
+        calls = [call(authurl='http://127.0.0.1:8080/auth/v1.0', user='test:tester', key='testing')]
+        swiftclient.client.Connection.assert_has_calls(calls)
+
+    @patch("swiftsuru.swift.swiftclient")
+    def test_init_should_call_get_token(self, swiftclient):
+        swiftclient.client.Connection.return_value.get_auth.return_value = (self.url, self.token)
+        cli = SwiftClient()
+        swiftclient.client.Connection.return_value.get_token.assert_called_once()
+
+    @patch("swiftsuru.swift.swiftclient")
+    def test_init_should_get_client_with_url_and_token(self, swiftclient):
+        swiftclient.client.Connection.return_value.get_auth.return_value = (self.url, self.token)
+        cli = SwiftClient()
+        swiftclient.client.Connection.assert_called_with(preauthurl=self.url, preauthtoken=self.token)
+
+    def test_create_account_should_call_swiftclient_equivalent(self):
+        pass
