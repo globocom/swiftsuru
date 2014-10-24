3854054dc704022251f9c9120e560dd39b699c55
Flavia Missi <flaviamissi@gmail.com>
2014-10-24 15:26:17 -0200

    uses environment variables for confs

--- swiftsuru/conf.py
+++ swiftsuru/conf.py
@@ -1,4 +1,6 @@
+from os import environ
+
 # swift stuff needed to perform operations on it
-AUTH_URL="http://127.0.0.1:8080/auth/v1.0"
-USER="test:tester"
-KEY="testing"
+AUTH_URL=environ.get("SWIFT_AUTH_URL", "http://127.0.0.1:8080/auth/v1.0")
+USER=environ.get("SWIFT_USER", "test:tester")
+KEY=environ.get("SWIFT_KEY", "testing")
