9daa048a66349f0b9f6a2faabd706e86c64a0b46
Flavia Missi <flaviamissi@gmail.com>
2014-10-27 17:56:00 -0200

    stops creating and removing account on bind/unbind. the api will only create/remove containers

--- swiftsuru/api.py
+++ swiftsuru/api.py
@@ -23,15 +23,9 @@ def bind(instance_name):
     app_name = app_host.split(".")[0]
 
     client = SwiftClient()
-    client.create_account({ACCOUNT_META_ITEM: app_name, ACCOUNT_META_SUBJECT: "team-name-here"}) # get_team(app_name)
     client.create_container(CONTAINER_TEMPLATE_NAME, headers={"X-Container-Read": ".r:*"})
     return "", 201
 
 @api.route("/resources/<instance_name>/bind", methods=["DELETE"])
 def unbind(instance_name):
-    app_host = request.form.get("app-host")
-    app_name = app_host.split(".")[0]
-
-    client = SwiftClient()
-    client.remove_account({ACCOUNT_META_ITEM: app_name})
     return "", 200
