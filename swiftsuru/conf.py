0c80b612cea718b8f04a0c07233372dc22b5deff
Alan <alan@corp.globo.com>
2014-11-18 15:17:38 -0200

    mongodb conf and dependencies

--- swiftsuru/conf.py
+++ swiftsuru/conf.py
@@ -7,3 +7,8 @@ PORT = int(environ.get("PORT", "8888"))
 AUTH_URL = environ.get("SWIFT_AUTH_URL", "http://127.0.0.1:8080/auth/v1")
 USER = environ.get("SWIFT_USER", "test:tester")
 KEY = environ.get("SWIFT_KEY", "testing")
+
+MONGO_USER = environ.get("MONGO_USER", "")
+MONGO_PASSWORD = environ.get("MONGO_PASSWORD", "")
+MONGO_HOST = environ.get("MONGO_HOST", "localhost:27017")
+MONGO_DATABASE = environ.get("MONGO_DATABASE", "swiftsuru")
