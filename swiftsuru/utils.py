4a64b6c1cb8d949bc41ec568803a513cf4a360f9
Flavia Missi <flaviamissi@gmail.com>
2014-12-15 13:54:24 -0200

    adds logging in requests to aclapi

--- swiftsuru/utils.py
+++ swiftsuru/utils.py
@@ -4,6 +4,7 @@ Swiftsuru related services to Tsuru
 import os
 import random
 import socket
+import syslog
 from aclapiclient import Client, L4Opts
 
 
@@ -39,20 +40,26 @@ def aclapi_cli():
 
 
 def permit_keystone_access(unit_host):
+    syslog.syslog("Permitting access to keystone host...")
+    syslog.syslog("Host is: {} port: ; unit host is: {}".format(conf.KEYSTONE_HOST, conf.KEYSTONE_PORT, unit_host))
     l4_opts = L4Opts("eq", conf.KEYSTONE_PORT, "dest")
-    aclapi_cli().add_tcp_permit_access(
+    resp = aclapi_cli().add_tcp_permit_access(
         desc="keystone access (swift service) for tsuru unit: {}".format(unit_host),
         source="{}/24".format(unit_host),
         dest="{}/32".format(socket.gethostbyname(conf.KEYSTONE_HOST)),
         l4_opts=l4_opts
     )
+    syslog.syslog("Response is: {} - {}".format(resp.status_code, resp.content))
 
 
 def permit_swift_access(unit_host):
+    syslog.syslog("Permitting access to swift host...")
+    syslog.syslog("Host is: {} port: ; unit host is: {}".format(conf.SWIFT_API_HOST, conf.SWIFT_API_PORT, unit_host))
     l4_opts = L4Opts("eq", conf.SWIFT_API_PORT, "dest")
-    aclapi_cli().add_tcp_permit_access(
+    resp = aclapi_cli().add_tcp_permit_access(
         desc="swift api access (swift service) for tsuru unit: {}".format(unit_host),
         source="{}/24".format(unit_host),
         dest="{}/32".format(socket.gethostbyname(conf.SWIFT_API_HOST)),
         l4_opts=l4_opts
     )
+    syslog.syslog("Response is: {} - {}".format(resp.status_code, resp.content))
