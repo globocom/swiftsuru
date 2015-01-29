5f99e976532a52227e08998916182fad77258f6b
Caio Brentano dos Passos <caio.passos@corp.globo.com>
2015-01-29 17:12:59 -0200

    aclapi methods log updated to logger

--- swiftsuru/utils.py
+++ swiftsuru/utils.py
@@ -47,7 +47,7 @@ def format_for_network_mask(ip):
 
 
 aclcli = None
-
+logger = get_logger(__name__)
 
 def aclapi_cli():
     global aclcli
@@ -57,8 +57,10 @@ def aclapi_cli():
 
 
 def permit_keystone_access(unit_host):
-    syslog.syslog("Permitting access to keystone host...")
-    syslog.syslog("Host is: {} port: {}; unit host is: {}".format(conf.KEYSTONE_HOST, conf.KEYSTONE_PORT, unit_host))
+    global logger
+
+    logger.info("Permitting access to keystone host...")
+    logger.info("Host is: {} port: {}; unit host is: {}".format(conf.KEYSTONE_HOST, conf.KEYSTONE_PORT, unit_host))
     l4_opts = L4Opts("eq", conf.KEYSTONE_PORT, "dest")
     resp = aclapi_cli().add_tcp_permit_access(
         desc="keystone access (swift service) for tsuru unit: {}".format(unit_host),
@@ -66,12 +68,14 @@ def permit_keystone_access(unit_host):
         dest="{}/32".format(socket.gethostbyname(conf.KEYSTONE_HOST)),
         l4_opts=l4_opts
     )
-    syslog.syslog("Response is: {} - {}".format(resp.status_code, resp.content))
+    logger.info("Response is: {} - {}".format(resp.status_code, resp.content))
 
 
 def permit_swift_access(unit_host):
-    syslog.syslog("Permitting access to swift host...")
-    syslog.syslog("Host is: {} port: {}; unit host is: {}".format(conf.SWIFT_API_HOST, conf.SWIFT_API_PORT, unit_host))
+    global logger
+
+    logger.info("Permitting access to swift host...")
+    logger.info("Host is: {} port: {}; unit host is: {}".format(conf.SWIFT_API_HOST, conf.SWIFT_API_PORT, unit_host))
     l4_opts = L4Opts("eq", conf.SWIFT_API_PORT, "dest")
     resp = aclapi_cli().add_tcp_permit_access(
         desc="swift api access (swift service) for tsuru unit: {}".format(unit_host),
@@ -79,4 +83,4 @@ def permit_swift_access(unit_host):
         dest="{}/32".format(socket.gethostbyname(conf.SWIFT_API_HOST)),
         l4_opts=l4_opts
     )
-    syslog.syslog("Response is: {} - {}".format(resp.status_code, resp.content))
+    logger.info("Response is: {} - {}".format(resp.status_code, resp.content))
